54780083f6c8a3d922f17e9ccabd0ce656292eb3
Axel Haustant <noirbizarre@gmail.com>
2018-05-22 20:24:14 +0200

    Wait for DB

--- serve.sh
+++ serve.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+# Wait for DB
+set -e
+if [ -n "$CKAN_WAIT_FOR" ]; then
+    echo "Waiting for database"
+    dockerize -wait $CKAN_WAIT_FOR -timeout 120s
+fi
 CKAN_INI=$CKAN_CONFIG/production.ini $CKAN_VENV/bin/python /paster_api.py &
 ckan-paster serve $CKAN_CONFIG/production.ini
